afd41decd8e9e7933ea94a92e9fcd16acf82a0a5
jahnvisugar <79485441+jahnvisugar@users.noreply.github.com>
2022-04-28 18:06:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+song1 = "";
+song2 = "";
+function preload()
+{
+song1 = loadSound("music.mp3")
+song2 = loadSound("music1.mp3")
+}
+
+function setup()
+{
+canvas = createCanvas(600, 500);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw()
+{
+image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+song.play();
+}
+
+function stop()
+{
+song.stop();
+}
+
+function pause()
+{
+song.pause();
+}
